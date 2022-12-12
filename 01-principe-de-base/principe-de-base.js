0c744338c4dc11bb99be5e4381e6918d34032b19
Mattia A. Fritz <mattia.fritz@unige.ch>
2022-12-12 11:33:55 +0100

    Necessary files and first example

---
+++ 01-principe-de-base/principe-de-base.js
@@ -0,0 +1,6 @@
+document
+  .getElementById("btn-interactif")
+  .addEventListener("click", function () {
+    //Ajouter du texte à la slide
+    document.getElementById("target-element").innerHTML = "Hello World !";
+  });
